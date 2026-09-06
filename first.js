9e6c3bd34889a4726c60db7a125f17a7f259fa49
dakshchouhan975-commits <dakshchouhan975@gmail.com>
2026-09-06 21:45:18 +0530

    Learned my first code of java script variables

--- first.js
+++ first.js
@@ -1,5 +1,22 @@
 console.log("Hi Buddy"); 
-name ="Daksh";
-age =21;
+let name ="Daksh";
+let age =21;
 console.log(name);
 console.log(age);
+
+
+// Variable are casesensitive
+let fullname = "Daksh";
+let FULLNAME = "Daksh";
+console.log(fullname);
+console.log(FULLNAME);
+
+// reserved words can't be used
+// let console=("Daksh");
+// console.log(console);
+
+// it will show error
+// but we can use by capital letter
+
+let Console=("Daksh");
+console.log(Console);
