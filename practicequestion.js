7f81181894f15638f6d0ac4ac32b8416fdd81927
dakshchouhan975-commits <dakshchouhan975@gmail.com>
2026-09-08 22:34:18 +0530

    Practice Question

---
+++ practicequestion.js
@@ -0,0 +1,31 @@
+// Q1.) create a const object called "product" to store information ?
+
+const product ={
+    Name:"Parker Jotter Standard CT Ball Pen (Black)",
+    Rating:4,
+    offer:10,
+    Price:540,
+};
+console.log(product);
+
+// Q2.) create a const object called "profile" to store information ? 
+
+const Profile={
+    Username:"@DakshChouhan",
+    isfollow:true,
+    Posts:120,
+    Followers:3000,
+    following:20,
+};
+console.log(Profile);
+
+// Q2.) create a const object called "Post" to store information ? 
+
+
+const Post={
+    title:"Sky is the limit",
+    description:"dont keep your limit low",
+    likes:200,
+    Comment:40,
+    share:100,
+};
