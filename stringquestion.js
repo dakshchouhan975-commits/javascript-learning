10d4ed8022a3bea239879256433ef236850eabc9
dakshchouhan975-commits <dakshchouhan975@gmail.com>
2026-09-15 22:59:27 +0530

    one question from string and use of method

--- stringquestion.js
+++ stringquestion.js
@@ -1,9 +1,7 @@
 // Q1. Prompt the user to enter their full name. Generate a username for them based on the input.
 // Start username with @, followed by their full name and ending with the full name length.
 
-// Example:
-// User name = "shradhakhapra"
-// Username should be = "@shradhakhapra13"
+
 
 let fullname=prompt("enter full name without spaces");
 let username="@"+fullname+fullname.length;
