df912dccf8da5440d96c063cf9afd5f14c1ad962
dakshchouhan975-commits <dakshchouhan975@gmail.com>
2026-09-17 23:25:47 +0530

    arrays in java script

---
+++ arrays.js
@@ -0,0 +1,24 @@
+// array i a collection of items 
+let marks=[71,45,31,78,90];
+ console.log(marks);
+
+let brands=["Nike","Puma","Dior","Gucci"];
+// console.log(brands);
+brands[0]="Polo";
+console.log(brands);
+ 
+
+// looping over an array  print all element of array
+
+let NUMBERS = [1,2,3,4,5,6,7];
+
+for(i=0;i<NUMBERS.length;i++){
+    console.log(NUMBERS[i]);
+}
+// also use for-of loop
+
+let Cars=["Audi","Porshe","G-Wagon","Lambo"];
+for(let el of Cars){
+console.log(el.toLocaleUpperCase());
+}
+ 
