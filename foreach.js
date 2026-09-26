81347f81f7f6b1d44850ba8a6b1179e854f420eb
dakshchouhan975-commits <dakshchouhan975@gmail.com>
2026-09-26 17:36:38 +0530

    foreach in js

---
+++ foreach.js
@@ -0,0 +1,19 @@
+
+let arr=[1,2,3,4,5,6,7];
+
+// arr.forEach(function height(val){
+//     console.log(val);
+// });
+
+arr.forEach((val)=>{
+    console.log(val);
+});
+
+
+//  use for each and show square of 
+
+
+let square=[10,4,80,7,90];
+square.forEachumber((num)=>{
+    console.log(num*num);
+});
